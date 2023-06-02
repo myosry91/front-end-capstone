21a7f8ffc481cebfbc7a0cc1c89e7773892ad3cb
Mahmoud Yosry <m.yosry91@yahoo.com>
2023-06-02 20:23:50 +0300

    semantic and meta tags

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <main>
+      <div></div>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import logo from '../assents/logo.png'
+function Nav() {
+  return (
+    <nav>
+          <img src={logo} alt='logo' />
+          <ul>
+              <li><a href=''>Home</a></li>
+              <li><a href=''>About</a></li>
+              <li><a href=''>Menu</a></li>
+              <li><a href=''>Reservation</a></li>
+              <li><a href=''>Order Online</a></li>
+              <li><a href=''>Login</a></li>
+          </ul>
+    </nav>
+  );
+}
+
+export default Nav;
